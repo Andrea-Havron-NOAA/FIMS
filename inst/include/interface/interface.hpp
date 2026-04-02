2998e640c7be370f8ae85c3a8fc419659e05a115
Andrea-Havron-NOAA <85530309+Andrea-Havron-NOAA@users.noreply.github.com>
2026-04-02 16:59:34 +0000

    swicth undef back to def

--- inst/include/interface/interface.hpp
+++ inst/include/interface/interface.hpp
@@ -24,31 +24,7 @@
 // issue
 #include <math.h>
 
-#ifdef FIMS_WINDOWS
-#ifdef FALSE
-#pragma push_macro("FALSE")
-#undef FALSE
-#define FIMS_RESTORE_FALSE_MACRO
-#endif
-#ifdef TRUE
-#pragma push_macro("TRUE")
-#undef TRUE
-#define FIMS_RESTORE_TRUE_MACRO
-#endif
-#endif
-
 #include <TMB.hpp>
-
-#ifdef FIMS_WINDOWS
-#ifdef FIMS_RESTORE_TRUE_MACRO
-#pragma pop_macro("TRUE")
-#undef FIMS_RESTORE_TRUE_MACRO
-#endif
-#ifdef FIMS_RESTORE_FALSE_MACRO
-#pragma pop_macro("FALSE")
-#undef FIMS_RESTORE_FALSE_MACRO
-#endif
-#endif
 
 // define REPORT, ADREPORT, and SIMULATE
 #define FIMS_REPORT_F(name, F)                                         \
